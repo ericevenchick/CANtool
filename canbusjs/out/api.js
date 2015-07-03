a2293e75c7ab8580dd87144142879a76c553934e
Eric Evenchick <eric@evenchick.com>
2015-07-03 17:09:44 -0400

    added more docs

---
+++ canbusjs/out/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "canbus.frame",
+        "canbus.slcan"
+    ],
+    "modules": [
+        "canbus"
+    ],
+    "allModules": [
+        {
+            "displayName": "canbus",
+            "name": "canbus"
+        }
+    ]
+} };
+});
